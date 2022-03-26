9ecb6edf808af26c5ba30388b5795c7538a3ed3d
Komal Grover <kogrover@microsoft.com>
2022-03-26 19:18:03 +0530

    updating url paths

--- order/urls.py
+++ order/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path("login/", views.Login, name="Login"),
+    path("index/", views.index, name="index"),
     path("register/", views.register, name="register"),
-    path("addItem/",views.register, name = "addItem"),
-    path("history/",views.register,name = "history")
-    #path("logout/", views.Logout, name="logout"),
+    path("addItem/",views.addItem, name = "addItem"),
+    path("history/",views.history,name = "history"),
+    path("logout/", views.Logout, name="logout"),
 ]
